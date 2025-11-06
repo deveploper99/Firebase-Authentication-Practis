8b6cc63a79212d7a21b35522f45008f51ac16243
deveploper99 <rtxdeveploper99@gmail.com>
2025-11-06 22:10:25 +0600

    firebase auth code add register Activity

--- app/src/main/java/com/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -7,14 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebaseauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText
+        }
 
 
         binding.registerText.setOnClickListener {
--- app/src/main/java/com/example/firebaseauth/ResigterActiviti.kt
+++ app/src/main/java/com/example/firebaseauth/ResigterActiviti.kt
@@ -2,14 +2,19 @@ package com.example.firebaseauth
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebaseauth.databinding.ActivityResigterBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class ResigterActiviti : AppCompatActivity() {
     private lateinit var binding: ActivityResigterBinding
+    private var auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +24,41 @@ class ResigterActiviti : AppCompatActivity() {
         binding.signIn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+
+        binding.RegiButton.setOnClickListener {
+
+            val email = binding.emailEditText.text.toString().trim()
+            val passW = binding.passwordEditText.text.toString().trim()
+            val cmPassW = binding.confirmPasswordEditText.text.toString().trim()
+
+            if (email.isEmpty() || passW.isEmpty() || cmPassW.isEmpty()){
+                Toast.makeText(this,"please fill all field", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            if (passW != cmPassW){
+                Toast.makeText(this, "Please Comfirm Password", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email,passW)
+                .addOnCompleteListener{ task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this,"Registration is Successful", Toast.LENGTH_SHORT)
+                    }else{
+                        Toast.makeText(this,"Registration is Not Successfull", Toast.LENGTH_SHORT)
+                    }
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+
+                }
+
+
+
+
+
         }
 
     }
